39f390dcd0dee9dc5d2ac1ef178ce07833b2dd82
Nandan Tadi <tadi.nandan@gmail.com>
2022-12-03 17:00:47 -0500

    new activity added

--- app/src/main/java/com/project5/rupizzeriamobile/MainActivity.java
+++ app/src/main/java/com/project5/rupizzeriamobile/MainActivity.java
@@ -2,7 +2,10 @@ package com.project5.rupizzeriamobile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -12,6 +15,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Button menuBTN = findViewById(R.id.menuBTN);
+        menuBTN.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, MenuActivity.class);
+            startActivity(intent);
+        });
 
     }
 
