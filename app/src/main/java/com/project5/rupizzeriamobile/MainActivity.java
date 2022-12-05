447822243635cb9a9ccb691369e99f72d58a0866
Nandan Tadi <tadi.nandan@gmail.com>
2022-12-05 15:59:39 -0500

    card view pizzas xml

--- app/src/main/java/com/project5/rupizzeriamobile/MainActivity.java
+++ app/src/main/java/com/project5/rupizzeriamobile/MainActivity.java
@@ -3,10 +3,8 @@ package com.project5.rupizzeriamobile;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 
 
@@ -20,7 +18,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
 
         Button menuBTN = findViewById(R.id.menuBTN);
         menuBTN.setOnClickListener(v -> {
@@ -45,7 +42,4 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
-
-
-
 }
--- app/src/main/java/com/project5/rupizzeriamobile/MenuActivity.java
+++ app/src/main/java/com/project5/rupizzeriamobile/MenuActivity.java
@@ -3,12 +3,18 @@ package com.project5.rupizzeriamobile;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class MenuActivity extends AppCompatActivity {
+
+    private RecyclerView menuScroll;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu);
+        menuScroll.findViewById(R.id.menuScroll);
 
+        menuScroll.add
     }
 }
