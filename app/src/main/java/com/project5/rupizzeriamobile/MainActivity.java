f65e06baa43dee24fb21e533854a9621e1506f76
Santosh <santoshmaryala@gmail.com>
2022-12-05 15:30:06 -0500

    menu.xml altered

--- app/src/main/java/com/project5/rupizzeriamobile/MainActivity.java
+++ app/src/main/java/com/project5/rupizzeriamobile/MainActivity.java
@@ -1,6 +1,8 @@
 package com.project5.rupizzeriamobile;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,6 +12,9 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RecyclerView recyclerView;
+
+    private RecyclerView.LayoutManager layoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +26,10 @@ public class MainActivity extends AppCompatActivity {
         menuBTN.setOnClickListener(v -> {
             Intent intent = new Intent(MainActivity.this, MenuActivity.class);
             startActivity(intent);
+            recyclerView = findViewById(R.id.menuScroll);
+            layoutManager = new GridLayoutManager(this,2);
+            recyclerView.setHasFixedSize(true);
+            recyclerView.setLayoutManager(layoutManager);
         });
 
         Button currentOrdersBTN = findViewById(R.id.currentOrdersBTN);
@@ -36,5 +45,7 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+
+
 
 }
