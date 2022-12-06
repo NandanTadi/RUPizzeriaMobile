0e45ec18dc6189deafffa5c5928ff4bdc41dfab4
Santosh <santoshmaryala@gmail.com>
2022-12-06 18:07:00 -0500

    listview set

--- app/src/main/java/com/project5/rupizzeriamobile/CurrentOrdersActivity.java
+++ app/src/main/java/com/project5/rupizzeriamobile/CurrentOrdersActivity.java
@@ -1,25 +1,57 @@
 package com.project5.rupizzeriamobile;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class CurrentOrdersActivity extends AppCompatActivity {
-    ArrayAdapter<String> adapter;
+import java.util.Collections;
+
+public class CurrentOrdersActivity extends AppCompatActivity implements AdapterView.OnItemClickListener{
+    private ListView listview;
+    private ArrayAdapter<Object> adapter;
+
+    /**
+     * Initial setup for the Views and the adapter for the ListView
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.current_activity);
-/*        adapter=new ArrayAdapter<String>(this, R.id.current_order_list,
-                MainActivity.pizzas);
-        setListAdapter(adapter);
+        adapter = new ArrayAdapter<Object>(this, android.R.layout.simple_list_item_1, MainActivity.pizzas.toArray());
+        listview = findViewById(R.id.current_order_list);
+        System.out.println(listview);
+        listview.setOnItemClickListener(this); //register the listener for an OnItemClick event.
+        listview.setAdapter(adapter);
     }
-    public void addItems(View v) {
-        listItems.add("Clicked : "+clickCounter++);
-        adapter.notifyDataSetChanged();
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+        alert.setTitle("Would you like to remove this pizza?");
+        alert.setMessage(adapterView.getAdapter().getItem(i).toString());
+        //anonymous inner class to handle the onClick event of YES or NO.
+        alert.setPositiveButton("yes", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                MainActivity.pizzas.remove(i);
+                System.out.println(MainActivity.pizzas);
+                Toast.makeText(getApplicationContext(), "Successfully Removed!", Toast.LENGTH_LONG).show();
+            }
+        }).setNegativeButton("no", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(getApplicationContext(), "Pizza Was Not Removed!", Toast.LENGTH_LONG).show();
+            }
+        });
+        AlertDialog dialog = alert.create();
+        dialog.show();
     }
-    }*/
 }
